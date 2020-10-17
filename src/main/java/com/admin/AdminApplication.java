1395f7cc2fa6056d49c973f81c445d9b1590590a
srikanth <srikanth2121098@gmail.com>
2020-10-17 20:42:13 +0530

    refactored code

--- src/main/java/com/admin/AdminApplication.java
+++ src/main/java/com/admin/AdminApplication.java
@@ -2,12 +2,14 @@ package com.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableMongoRepositories(basePackages = {"org.models"})
+@ComponentScan(basePackages = {"com.admin,org.models"})
 public class AdminApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
--- src/main/java/com/admin/controller/CarsController.java
+++ src/main/java/com/admin/controller/CarsController.java
@@ -5,12 +5,17 @@ package com.admin.controller;
 import org.models.core.dao.SearchRepository;
 import org.models.core.dao.VehicleRepository;
 import org.models.core.domain.Vehicle;
+import org.models.core.error.model.MakeError;
+import org.models.core.properies.VehicleProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Example;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/vehicle")
@@ -23,11 +28,12 @@ public class CarsController {
     SearchRepository searchRepository;
 
 
+    @Autowired
+    VehicleProperties vehicleProperties;
 
 
     @PostMapping("/add")
-    public Vehicle add(@RequestBody Vehicle car){
-        System.out.println(car.getMake());
+    public Vehicle add(@RequestBody @Valid Vehicle car){
         return autoMobileRepository.save(car);
     }
 
