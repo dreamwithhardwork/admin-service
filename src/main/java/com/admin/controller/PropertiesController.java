1a998e55780e42e6525aa7c35fb7e77f6876aefc
srikanth <srikanth2121098@gmail.com>
2020-10-30 20:32:02 +0530

    properties controller

---
+++ src/main/java/com/admin/controller/PropertiesController.java
@@ -0,0 +1,31 @@
+package com.admin.controller;
+
+import com.admin.AdminApplication;
+import org.models.core.dao.AvailableCarPropertiesRepository;
+import org.models.core.domain.AvailableCarProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(AdminApplication.PATH+"/properties")
+public class PropertiesController {
+
+
+    @Autowired
+    AvailableCarPropertiesRepository availableCarPropertiesRepository;
+
+
+    @PostMapping("/add/car")
+    public AvailableCarProperties add(@RequestBody  AvailableCarProperties availableCarProperties){
+        return   availableCarPropertiesRepository.save(availableCarProperties);
+    }
+
+    @GetMapping("/get/car")
+    public List<AvailableCarProperties> get(){
+        return availableCarPropertiesRepository.findAll();
+    }
+
+
+}
